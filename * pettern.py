72e4350e71bf59baa68c12ba189d132433b8de91
shanti96 <106430039+shanti96@users.noreply.github.com>
2022-08-05 17:43:31 +0530

    Add files via upload

---
+++ * pettern.py
@@ -0,0 +1,13 @@
+i=1
+while i<5:
+    j=1
+    while j<5-i:
+        print(" ",end="")
+        j=j+1
+    k=1
+    while k<=i: 
+        print("*",end="")
+        print(" ",end="") 
+        k=k+1
+    i=i+1
+    print()                       
